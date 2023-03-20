76543a739137121e492696ebc21ecb173134a4f2
Pierre PINTARC <pierre@redtrash.fr>
2023-03-20 10:15:37 +0100

    Upgrade to GLPI 10.0.6 Upgrade of all plugins

---
+++ glpi-cron-daemon/downstream.php
@@ -0,0 +1,4 @@
+<?php
+define('GLPI_CONFIG_DIR', '/var/glpi/config');
+define('GLPI_VAR_DIR', '/var/glpi/files');
+?>
---
+++ glpi-cron-daemon/start_glpi-cron-daemon.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Modify schedule of CRON config file
+if [ -z "${CRON_SCHEDULE}" ]
+then
+    CRON_SCHEDULE="*/2 * * * *"
+fi
+sed -i "s|%%SCHEDULE%%|${CRON_SCHEDULE}|" /etc/crontabs/glpi.cron
+
+# Modify default timezone for PHP
+sed -i "s|;date.timezone =|date.timezone=${TZ}|" /etc/php81/php.ini
+
+# Modify default cookie_httponly value for security purpose
+sed -i "s|session.cookie_httponly =|session.cookie_httponly = 1|" /etc/php81/php.ini
+
+# Waiting for the installation to be done
+echo `date` " - Waiting GLPI to be installed from ppcm/glpi-server"
+done_file='/var/glpi/config/.installation_done'
+while [ ! -f "$done_file" ]
+do
+    inotifywait -qq -t 30 -e create -e moved_to "$(dirname $done_file)"
+done
+
+echo `date` " - Start CRON job"
+
+# Run GLPI cron script
+/usr/sbin/crond -f
