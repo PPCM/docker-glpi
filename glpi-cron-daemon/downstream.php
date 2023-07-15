cc7119381903f1cd21a828ebc4534ace68aa64e2
Pierre PINTARC <pierre@redtrash.fr>
2023-07-15 17:54:56 +0200

    - Correct missing Database configuration file - Upgrade GLPI to 10.0.9 - Upgrade news plugin to 1.2.1 - Add php81-xmlreader package to be compliant with GLPI 10.0.9 - Add php81-xmlwriter package to be compliant with GLPI 10.0.9

--- glpi-cron-daemon/downstream.php
+++ glpi-cron-daemon/downstream.php
@@ -1,5 +1,5 @@
 <?php
-define('GLPI_CONFIG_DIR', '/etc/glpi');
+define('GLPI_CONFIG_DIR', '/etc/glpi/');
 
 if (file_exists(GLPI_CONFIG_DIR . '/local_define.php')) {
    require_once GLPI_CONFIG_DIR . '/local_define.php';
