3fc4c5de9e4d249a45ee5bc445ee04de7e8de053
Pierre PINTARC <pierre@redtrash.fr>
2023-12-24 14:44:54 +0100

    - start_glpi-server.sh: MYSQL_ROOT_PASSWORD is no more mandatory, if you already configured the user and the database, the variable could ignored
    - start_glpi-server.sh: Quote values everywhere to allow special caracters
    - README.md: Fix path for config files
    - README.md: Fix path for downloaded files
    
    - Update Alpine to 3.19
    - Update PHP to 8.2
    - Update GLPI to 10.0.11

--- glpi-cron/start_glpi-cron.sh
+++ glpi-cron/start_glpi-cron.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Modify default timezone for PHP
-sed -i "s|;date.timezone =|date.timezone=${TZ}|" /etc/php81/php.ini
+sed -i "s|;date.timezone =|date.timezone=${TZ}|" /etc/php82/php.ini
 
 # Modify default cookie_httponly value for security purpose
-sed -i "s|session.cookie_httponly =|session.cookie_httponly = 1|" /etc/php81/php.ini
+sed -i "s|session.cookie_httponly =|session.cookie_httponly = 1|" /etc/php82/php.ini
 
 # Waiting for the installation to be done
 echo `date` " - Waiting GLPI to be installed from ppcm/glpi-server"
